531ce78c8af11789843ea420be53d821479edaee
ducminh1012 <ducminh1012@gmail.com>
2016-10-04 21:13:42 +0700

    Validate the text input

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -95,6 +95,7 @@ class ViewController: UIViewController {
         self.moneyPerThreeLabel.text = formatWithCurrency(price: priceZero)
         self.moneyPerFourLabel.text = formatWithCurrency(price: priceZero)
         
+        self.moneyTextField.delegate = self
 //        self.resultView.alpha = 0
         
     }
@@ -144,6 +145,37 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let array = Array(textField.text!.characters)
+        
+        switch string {
+        case "0","1","2","3","4","5","6","7","8","9":
+            return true
+        case ".":
+            
+            var decimalCount = 0
+            for character in array {
+                if character == "." {
+                    decimalCount += 1
+                }
+            }
+            
+            if decimalCount == 1 {
+                return false
+            } else {
+                return true
+            }
+        default:
+            let array = Array(string.characters)
+            if array.count == 0 {
+                return true
+            }
+            return false
+        }
+    }
+    
 }
 
 // Utils
